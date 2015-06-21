9d1c1ae5c4fcf854fe5024e93dcb06575c5f874f
cmondies <cmondies@mac.com>
2015-06-21 19:25:05 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-service tor start & sleep 10; service tor stop ; echo "Your hostname:" ; cat /var/lib/tor/hidden_service/hostname
+#service tor start & sleep 10; service tor stop ; echo "Your hostname:" ; cat /var/lib/tor/hidden_service/hostname
+service tor start
