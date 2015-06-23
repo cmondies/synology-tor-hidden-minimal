232526913499ec31c683affd3f13cd11b8533ec3
cmondies <cmondies@mac.com>
2015-06-23 12:47:44 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-#exec sudo -u tor /usr/bin/tor -f /etc/tor/torrc ;
-#exec sudo -u root lighttpd -f /etc/lighttpd/lighttpd.conf ;
+sleep 10
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
-#service tor start
-#service lighttpd start
+exit
 
