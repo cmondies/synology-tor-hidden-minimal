d3915bc099e105dd355492759f590d0e48b46c31
cmondies <cmondies@mac.com>
2015-06-24 09:32:22 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-sleep 30
+sleep 5
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
-sleep 60
-echo "Your hostname :"
-cat /var/lib/tor/hidden_service/hostname
 exit
 
